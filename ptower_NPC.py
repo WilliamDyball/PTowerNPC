3f18d5a58c729aaf63791da0f7cbc7932ca2dca9
William Dyball <sherberted@hotmail.co.uk>
2016-01-28 10:48:36 +0000

    Searches the incoming line for the wounds and set the value

--- ptower_NPC.py
+++ ptower_NPC.py
@@ -1,4 +1,4 @@
-import os, pygame, string, thread, sys, getopt, time
+import os, pygame, string, thread, sys, getopt, time, re
 from pygame.locals import *
 from socket import *
 
@@ -13,150 +13,162 @@ serverName      = "localhost"
 portNumber      = 7000
 programName     = "ptower_NPC"
 connectAttempts = 10
+wounds          = 10
+fatigue         = 100
+cRoom           = 0
 
 
 def connectServer(id, count):
     print "connectServer called"
     global sckt, serverName, portNumber
 
-    # s = open('testscript', 'r')
-    # line = s.readline()
-    while True:
-        sckt = socket(AF_INET, SOCK_STREAM)
-        print "attempting to connect to %s:%d" % (serverName, portNumber)
-        try:
-            sckt.connect((serverName, portNumber))
-            break
-        except error, why:
-            print "cannot find server on %s:%d" % (serverName, portNumber)
-            count -= 1
-            if count>0:
-                print "will try again"
-                portNumber += 1
-            else:
-                print "cannot connect to %s:%d the server is probably not running or it is not accessible" % (serverName, portNumber)
-                sys.exit(0)
-        
-    #f = sckt.makefile("rb")
-    #line = stripcrlf(f.readline())
-    #while line:
-        #print line
-        # line = s.readline()
-        #line = stripcrlf(f.readline())
-        #print "line =", line, " thus stopping"
+    sckt = socket(AF_INET, SOCK_STREAM)
+
+    sckt.connect((serverName, portNumber))
 
 def setUpName(): #Bypasses the start screen and then sets the name of the bot to Bot
     global sckt
-    print('\nSetting name to Bot. \n')
+    print '\nSetting name to Bot. \n'
     time.sleep(0.25)
     sckt.send('\n')
     time.sleep(0.25)
     sckt.send('Bot')
     sckt.send('\n')
-    print('Name has been set and should be in game.\n')
-        
+    print 'Name has been set and should be in game.\n'
+
+
+def processLine(line):
+    global wounds
+    #
+    #Searches the input line for "dW " and then searches the result for an integer and then the global wounds is set the value found
+    #
+    m = re.search("(dW .*)", line)
+    if m:
+        print("search.....", m.group(1))
+        tempdW = m.group(1)
+        tempdW1 = re.search(r"([0-9]+)", tempdW)
+        wounds = tempdW1.group(1)
+        print "search......", wounds
+    
+    
+    
 def step1():
     global sckt
     sckt.send('1')
-    print('Stepping forward 1')
+    print 'Stepping forward 1'
 
-def setp2():
+def step2():
     global sckt
     sckt.send('2')
-    print('Stepping forward 2')
+    print 'Stepping forward 2'
     
 def turnLeft():
     global sckt
     sckt.send('l')
-    print('Turning left')
+    print 'Turning left'
 
 def turnRight():
     global sckt
     sckt.send('r')
-    print('Turn right')
+    print 'Turn right'
 
 def vault():
     global sckt
     sckt.send('v')
-    print('Vaulting')
+    print 'Vaulting'
     
 def attack():
     global sckt
     sckt.send('a')
-    print('Attacking')
+    print 'Attacking'
 
 def openDoor():
     global sckt
     sckt.send('o')
-    print('Opening door')
+    print 'Opening door'
 
 def fireNArrow():
     global sckt
     sckt.send('f')
-    print('Firing a normal arrow')
+    print 'Firing a normal arrow'
 
 def fireMArrow():
     global sckt
     sckt.send('m')
-    print('Firing a magic arrow')
+    print 'Firing a magic arrow'
 
 def parry():
     global sckt
     sckt.send('p')
-    print('Parrying')
+    print 'Parrying'
 
 def thrust():
     global sckt
     sckt.send('t')
-    print('Thrusting')
+    print 'Thrusting'
 
 def closeDoor():
     global sckt
     sckt.send('c')
-    print('Closing door')
+    print 'Closing door'
 
 def examineDoor():
     global sckt
     sckt.send('e')
-    print('Examining door')
+    print 'Examining door'
 
 def getTreasure():
     global sckt
     sckt.send('g')
-    print('Getting treasure')
+    print 'Getting treasure'
 
 def dropTreasure():
     global sckt
     sckt.send('d')
-    print('Dropping treasure')
+    print 'Dropping treasure'
 
 def useTreasure():
     global sckt
     sckt.send('u')
-    print('Using treasure')
+    print 'Using treasure'
 
 def speak():
     global sckt
     sckt.send('s')
-    print('Speaking')
+    print 'Speaking'
+
+def stripclrf(s):
+    return s[:-1]
     
 def initEventLoop():
     print "initEventLoop called"
-    global sckt
+    global sckt, wounds
 
-    thread.start_new(connectServer, (1,connectAttempts))
+    #thread.start_new(connectServer, (1,connectAttempts))
 
+    connectServer(1, connectAttempts)
     setUpName()
+
+    f = sckt.makefile("rb")
+    line = stripclrf(f.readline())
     
     while True:
+        line = sckt.recv(1024)
+#        print "Processing line", line, "\n"
+        processLine(line)
         time.sleep(1)
+        print "Wounds: ", wounds
+        if wounds < 75:
+            vault()
+            step2()
+            print "AAAAAHHHH!!!"
         step1()
-        time.sleep(1)
-        turnLeft()
-        time.sleep(1)
-        turnRight()
-        time.sleep(1)
-        attack()
+#        time.sleep(1)
+#        turnLeft()
+#        time.sleep(1)
+#        turnRight()
+#        time.sleep(1)
+#        attack()
 
 def initNPC():
     print "initNPC called"
